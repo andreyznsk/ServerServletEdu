26d6b07b796df3963fde07da635b8b42d22225d3
andreyz <andreyznsk@gmail.com>
2022-08-09 20:20:03 +0700

    Start taskLesson1

---
+++ src/main/java/ru/sbt/servlet/DemoServlet.java
@@ -0,0 +1,24 @@
+package ru.sbt.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class DemoServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        res.setContentType("text/html");//setting the content type
+        PrintWriter pw = res.getWriter();//get the stream to write the data
+
+//writing html in the stream
+        pw.println("<html><body>");
+        pw.println("Welcome to servlet");
+        pw.println("</body></html>");
+
+        pw.close();//closing the stream
+    }
+}
